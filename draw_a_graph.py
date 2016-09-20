1c13757d7a8d06c1f8f35201c13759e89cba8b7f
gggsanna <giangiacomo.sanna@gmail.com>
2016-09-20 14:52:47 +0200

    typo line 4

--- draw_a_graph.py
+++ draw_a_graph.py
@@ -1,7 +1,7 @@
 # A student's packagies
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib import LinearLocator, FormatStrFormatter
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 
 

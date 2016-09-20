0b101b60bcbeb447e98d4c503983cc06294a8d18
gggsanna <giangiacomo.sanna@gmail.com>
2016-09-20 14:38:46 +0200

    typo line 2

--- draw_a_graph.py
+++ draw_a_graph.py
@@ -1,5 +1,5 @@
 # A student's packagies
-from mpl_toolkits.mplot3d import Axex3D
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
